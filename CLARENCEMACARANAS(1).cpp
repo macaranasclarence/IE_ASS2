e647f2d4270cb3c7179b9aab94710b66411b6091
macaranasclarence <42115512+macaranasclarence@users.noreply.github.com>
2018-08-06 18:12:53 +0800

    Add files via upload

---
+++ CLARENCEMACARANAS(1).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+
+    cout<<"Steps on Washing Clothes Mannually" << endl;
+    cout<<"Step 1. Separate white shirts from colored shirts." << endl;
+    cout<<"Step 2. Soak the clothes in water wit fabric detergent." << endl;
+    cout<<"Step 3. Wash the clothes by hand." << endl; 
+    cout<<"Step 4. Rinse the clothes." << endl;
+    cout<<"Step 5. Hang the clothes." << endl;
+    return 0;
+}
